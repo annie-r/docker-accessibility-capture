c31f937cdee329b9849446020937d574644abd04
Annie <ansross@cs.washington.edu>
2017-12-07 16:22:29 -0800

    moves screenshot files from emu to shared file

--- docker-accessibility-capture/docker/docker_emu_node.py
+++ docker-accessibility-capture/docker/docker_emu_node.py
@@ -1,13 +1,14 @@
 from networking import Network
 from message import *
 import os
+import shutil
 
 class EmuNode(Network):
 	def __init__(self, port, name, shared_path = "./shared/"):
 		Network.__init__(self,port,name)
 		self.shared_path = shared_path
 		self.ss_path = "./screenshots/"
-		self.ss_counter = { "Gmail":11}
+		self.ss_counter = { "Gmail":1}
 
 	def parse_message(self, data, addr=None):
 		msg = Message()
@@ -16,11 +17,13 @@ class EmuNode(Network):
 		if msg.type == TRAVERSAL_DATA:
 			# TODO retrieve data
 			self.parse_traversal(msg.fileName)
-			ss_file = self.take_screenshot(msg.appName)
 			# act on it
 			# return screenshot if there's still one to send
-			# else, ignore
-			if(ss_counter(msg.app) > 0):
+			ss_file = self.take_screenshot(msg.appName)
+
+			# send message once screenshot is ready, if there is one
+			# if we're out of screenshots, ignore
+			if(ss_file != None):
 				resp_msg = Message(self.name,SCREENSHOT_READY,msg.appName,ss_file)
 				self.send_message_to_node("serv", self.PORT, resp_msg.serializeMessage())
 
@@ -35,11 +38,19 @@ class EmuNode(Network):
 
 	def take_screenshot(self, app):
 		# for proj, take image from set of screenshot and move to shared folder
-		filename = app+str(self.ss_counter[app])
-		print ("moving : " +self.ss_path+filename + "to"+ self.shared_path+"images/"+filename)
-		os.rename(self.ss_path+filename,self.shared_path+"images/"+filename)
-		self.ss_counter[app] = self.ss_counter[app]-1
-		return filename
+
+		filename = app+str(self.ss_counter[app])+".jpg"
+		filepath = self.ss_path+filename
+		print ("filename: "+filename)
+		if (os.path.isfile(filepath)):
+			print ("moving : " +filepath + "to"+ self.shared_path+"images/"+filename)
+			shutil.move(self.ss_path+filename,self.shared_path+"images/"+filename)
+			self.ss_counter[app] = self.ss_counter[app]+1
+			return filename
+		#if run out of SS for that app
+		else:
+			print ("No file: "+filename)
+			return None
 
 if __name__ == '__main__':
     n = EmuNode(8888, "emu1")
